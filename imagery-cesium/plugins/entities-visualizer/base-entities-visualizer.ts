ba7f8c0f8e1c7e213813eec6af59cb363b74e688
nirko <nir.kovalio@gmail.com>
2020-04-02 09:55:09 +0300

    Add Geo coords to cesium coords class Cesium entities visualizer now support point, polyline and polygon

--- imagery-cesium/plugins/entities-visualizer/base-entities-visualizer.ts
+++ imagery-cesium/plugins/entities-visualizer/base-entities-visualizer.ts
@@ -1,5 +1,9 @@
 import { BaseImageryVisualizer, IVisualizerEntity, VisualizerInteractionTypes } from '@ansyn/imagery';
 import { Observable, of } from 'rxjs';
+import { Feature, GeometryObject, LineString, Point, Polygon } from 'geojson';
+import * as geoToCesium from '../utils/geoToCesium'
+import { IVisualizerStateStyle, IVisualizerStyle } from "../../../imagery/public_api";
+import { cloneDeep, merge } from 'lodash';
 
 declare const Cesium: any;
 
@@ -10,12 +14,13 @@ export abstract class BaseEntitiesVisualizer extends BaseImageryVisualizer {
 	public idToEntity: Map<string, any> = new Map<string, { feature: null, originalEntity: null }>();
 
 	onInit() {
-		this.billboardCollection = this.iMap.mapObject.scene.primitives.add(new Cesium.BillboardCollection());
+		// this.billboardCollection = this.iMap.mapObject.scene.primitives.add(new Cesium.BillboardCollection());
+
 	}
 
 	getIds(entity: IVisualizerEntity): string {
 		if (entity.icon) {
-			return `billboard_${ entity.id }`;
+			return `billboard_${entity.id}`;
 		}
 		return entity.id;
 	}
@@ -25,44 +30,212 @@ export abstract class BaseEntitiesVisualizer extends BaseImageryVisualizer {
 
 	addOrUpdateEntities(logicalEntities: IVisualizerEntity[]): Observable<boolean> {
 		logicalEntities.forEach((entity: IVisualizerEntity) => {
-			if (entity.icon) {
-				const id = this.getIds(entity);
-				const billboardEntity = {
-					show: true,
-					position: Cesium.Cartesian3.fromDegrees(
-						entity.featureJson.geometry.coordinates[0],
-						entity.featureJson.geometry.coordinates[1],
-						entity.featureJson.geometry.coordinates[2] ? entity.featureJson.geometry.coordinates[2] : 0
-					),
-					pixelOffset: Cesium.Cartesian2.ZERO,
-					eyeOffset: Cesium.Cartesian3.ZERO,
-					heightReference: Cesium.HeightReference.NONE,
-					horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
-					verticalOrigin: Cesium.VerticalOrigin.CENTER,
-					scale: 1.0,
-					image: entity.icon,
-					imageSubRegion: undefined,
-					color: Cesium.Color.WHITE,
-					id: id,
-					rotation: 0.0,
-					alignedAxis: Cesium.Cartesian3.ZERO,
-					width: undefined,
-					height: undefined,
-					scaleByDistance: undefined,
-					translucencyByDistance: undefined,
-					pixelOffsetScaleByDistance: undefined,
-					sizeInMeters: false,
-					distanceDisplayCondition: undefined
-				};
-				const feature = this.billboardCollection.add(billboardEntity);
-				this.idToEntity.set(id, <any>{
-					originalEntity: entity,
-					feature: feature
-				});
+			const featureJson: Feature<any> = entity.featureJson;
+
+			switch (featureJson.geometry.type) {
+				case 'Point': {
+					this.getOrCreateDataSource("Point").then(ds => {
+						if (entity.icon) {
+							ds.entities.add(this.getBillbord(entity.id, featureJson.geometry, entity.icon));
+						} else {
+							ds.entities.add(this.getPoint(entity.id, featureJson.geometry, entity.style));
+						}
+					});
+					break;
+				}
+				case 'LineString': {
+					this.getOrCreateDataSource("LineString").then(ds => {
+						ds.entities.add(this.getPolyline(entity.id, featureJson.geometry, entity.style));
+					});
+					break;
+				}
+				case 'Polygon': {
+					this.getOrCreateDataSource("Polygon").then(ds => {
+						ds.entities.add(this.getPolygon(entity.id, featureJson.geometry, entity.style));
+					});
+					break;
+				}
 			}
 		});
 		return of(true);
 	}
+
+
+	private getBillbord(id: string, geometry: Point, imgUrl: string): any {
+		return {
+			id: id,
+			position: geoToCesium.coordinatesToCartesian(geometry.coordinates),
+			billboard: {
+				image: imgUrl
+			}
+		};
+	}
+
+	private getPoint(id: string, geometry: Point, stylesState?: Partial<IVisualizerStateStyle>): any {
+		const styles = merge({}, stylesState);
+		const s: IVisualizerStyle = merge({}, styles.initial);
+		const ptColor = this.getColor(s["marker-color"]);
+		return {
+			id: id,
+			position: geoToCesium.coordinatesToCartesian(geometry.coordinates),
+			point: {
+				color: ptColor
+			}
+		}
+	}
+
+	private getPolyline(id: string, geometry: LineString, stylesState?: Partial<IVisualizerStateStyle>): any {
+
+		// TODO: Support all polyline styles
+		const styles = merge({}, stylesState);
+		const s: IVisualizerStyle = merge({}, styles.initial);
+
+		const lineColor = this.getColor(s["stroke"]);
+		const lineWidth = s['stroke-width'];
+
+		return {
+			id: id,
+			polyline: {
+				positions: geoToCesium.multiLineToCartesian(geometry.coordinates),
+				width: lineWidth,
+				material: lineColor
+			}
+		};
+	}
+
+	private getPolygon(id: string, geometry: Polygon, stylesState?: Partial<IVisualizerStateStyle>): any {
+
+		// TODO: Support all polygon styles
+		const styles = merge({}, stylesState);
+		const s: IVisualizerStyle = merge({}, styles.initial);
+
+		const lineColor = this.getColor(s["stroke"]);
+		const fillColor = this.getColor(s["fill"]);
+
+		const lineWidth = s['stroke-width'];
+
+		const poly = new Cesium.PolygonHierarchy(geoToCesium.polygonCoordinatesToCartesian(geometry.coordinates[0]));
+
+
+		// Adding holes
+		for (let i = 1; i < geometry.coordinates.length; i++) {
+			poly.holes.push(geoToCesium.polygonCoordinatesToCartesian(geometry.coordinates[i]));
+		}
+
+		return {
+			id: id,
+			polygon: {
+				hierarchy: poly,
+				material: fillColor,
+				outline: true,
+				outlineColor: lineColor,
+				outlineWidth: lineWidth,
+			}
+		};
+	}
+
+	private getColor(color: string = "RED"): Cesium.Color {
+		// Cesium Color Can't handle rrggbbaa so ...
+		const rrggbbaaMatcher = /^#([0-9a-f]{8})$/i;
+
+		const matches = rrggbbaaMatcher.exec(color);
+		if (matches !== null) {
+			const c = Cesium.Color.fromCssColorString(color.substring(0, 7));
+			c.alpha = parseInt(color.substring(7), 16) / 255;
+			return c;
+		} else {
+			return Cesium.Color.fromCssColorString(color);
+		}
+	}
+
+	/*
+	protected createStyle(feature: Feature, isStyle, ...styles: Array<Partial<IVisualizerStyle>>) {
+			const styleSettings: IVisualizerStyle = merge({}, ...styles);
+			this.fixStyleValues(feature, styleSettings);
+
+			let firstStyle: any = {};
+			let secondaryStyle: any = {};
+			let textStyle: any = {};
+
+			if (styleSettings.shadow) {
+				secondaryStyle.stroke = new Stroke({
+					color: styleSettings.shadow.stroke,
+					width: styleSettings.shadow['stroke-width']
+				});
+			}
+
+			if (styleSettings.stroke) {
+				const color = this.colorWithAlpha(styleSettings.stroke, styleSettings['stroke-opacity']);
+				const dash = styleSettings['stroke-dasharray'];
+				const lineDash = dash > 0 ? [dash, 10] : undefined;
+				const width = styleSettings['stroke-width'];
+				const lineCap = dash > 0 ? 'square' : undefined;
+
+				firstStyle.stroke = new Stroke({ color, lineDash, width, lineCap, lineDashOffset: 5 });
+			}
+
+			if (styleSettings.fill) {
+				const color = this.colorWithAlpha(styleSettings.fill, styleSettings['fill-opacity']);
+				firstStyle.fill = new Fill({ color });
+			}
+
+			if (styleSettings.icon) {
+				firstStyle.image = new Icon(styleSettings.icon);
+			}
+
+			if (styleSettings.circle) {
+				const radius = styleSettings.circle;
+				firstStyle.image = new Circle({
+					radius,
+					fill: firstStyle.fill,
+					stroke: firstStyle.stroke
+				});
+			}
+
+			if (styleSettings.geometry) {
+				secondaryStyle.image = firstStyle.image;
+				secondaryStyle.geometry = styleSettings.geometry
+			}
+
+			if ((styleSettings.label && styleSettings.label.text) && !feature.getProperties().labelTranslateOn) {
+				const fill = new Fill({ color: styleSettings.label.fill });
+				const stroke = new Stroke({
+					color: styleSettings.label.stroke ? styleSettings.label.stroke : '#fff',
+					width: styleSettings.label.stroke ? 4 : 0
+				});
+				const { label } = styleSettings;
+
+				textStyle.text = new Text({
+					overflow: label.overflow,
+					font: `${ styleSettings.label.fontSize }px Calibri,sans-serif`,
+					offsetY: <any>styleSettings.label.offsetY,
+					text: <any>label.text,
+					fill,
+					stroke
+				});
+				textStyle.geometry = (feature) => {
+					const { label } = feature.getProperties();
+					if (label.geometry) {
+						const oldCoordinates = label.geometry.getCoordinates();
+						const newCoordinates = [this.offset[0] + oldCoordinates[0], this.offset[1] + oldCoordinates[1]];
+						return new Point(newCoordinates);
+					}
+					return new Point(this.getCenterOfFeature(feature).coordinates)
+				};
+
+				firstStyle.geometry = (feature) => feature.getGeometry();
+			}
+
+			if (styleSettings['marker-color'] || styleSettings['marker-size']) {
+				const color = styleSettings['marker-color'];
+				const radius = MarkerSizeDic[styleSettings['marker-size']];
+				firstStyle.image = new Circle({ fill: new Fill({ color }), stroke: null, radius });
+			}
+
+
+			return [firstStyle, textStyle, secondaryStyle].map(style => isStyle ? new Style(style) : style);
+		}
+	 */
 
 	clearEntities() {
 		this.billboardCollection.removeAll();
@@ -109,4 +282,17 @@ export abstract class BaseEntitiesVisualizer extends BaseImageryVisualizer {
 
 	setVisibility(isVisible: boolean): void {
 	}
+
+	getOrCreateDataSource(id) {
+		return new Promise<Cesium.CustomDataSource>((resolve, reject) => {
+			const ds = this.iMap.mapObject.dataSources.getByName(id);
+			if (ds.length === 0) {
+				return this.iMap.mapObject.dataSources.add(new Cesium.CustomDataSource(id)).then(value => resolve(value));
+			} else {
+				return resolve(ds[0]);
+			}
+		});
+
+	}
+
 }
--- imagery-tester/src/app/plugins/cesium/mouse-marker-plugin.ts
+++ imagery-tester/src/app/plugins/cesium/mouse-marker-plugin.ts
@@ -57,6 +57,120 @@ export class MouseMarkerPlugin extends BaseEntitiesVisualizer {
       featureJson: point([position.long, position.lat, position.height])
     };
 
-    this.setEntities([entity]);
+    const polygon : IVisualizerEntity = {
+      id: 'Polygon',
+      featureJson: {
+        type: "Feature",
+        geometry: {
+          "type":"Polygon",
+          "coordinates":[
+            [
+              [
+                -79.9441543655779,
+                6.128701260566601
+              ],
+              [
+                -13.612496074120623,
+                47.529128645116856
+              ],
+              [
+                74.60638151695977,
+                -18.541194853701725
+              ],
+              [
+                -79.9441543655779,
+                6.128701260566601
+              ]
+            ]
+          ]
+        },
+        properties:{}
+      },
+      style:{
+        initial:{
+          "fill":"white",
+          "stroke":"#27b2cfe6",
+          "stroke-width":1,
+          "stroke-dasharray":0,
+          "fill-opacity":0.4,
+          "stroke-opacity":1,
+        }
+      }
+    }
+
+    const polyline : IVisualizerEntity = {
+      id: 'Polyline',
+      featureJson: {
+        type: "Feature",
+        geometry: {
+          type : "LineString",
+          coordinates: [
+            [
+              37.08545167242462,
+              29.723747917711435
+            ],
+            [
+              34.40537649183417,
+              35.36759607584702
+            ],
+            [
+              31.05530091865578,
+              31.834237068921382
+            ]
+          ]
+        },
+        properties:{}
+      },
+      style:{
+        initial:{
+          stroke:"#27b2cfe6",
+          "stroke-width":1,
+          "stroke-dasharray":0,
+        }
+      }
+    };
+
+    const billboardEntity : IVisualizerEntity = {
+      id: 'billboard',
+      icon: 'assets//logo.svg',
+      featureJson: {
+        type: "Feature",
+        geometry: {
+          type : "Point",
+          coordinates: [
+            -6.018961997487438,
+            31.26325432597315
+          ]
+        },
+        properties:{}
+      },
+      style:{
+        initial:{
+          "marker-color":"#ffffff",
+        }
+      }
+    };
+
+    const pointEntity : IVisualizerEntity = {
+      id: 'Point',
+      featureJson: {
+        type: "Feature",
+        geometry: {
+          type : "Point",
+          coordinates: [
+            -6.018961997487438,
+            31.26325432597315
+          ]
+        },
+        properties:{}
+      },
+      style:{
+        initial:{
+          "marker-color":"#ffffff",
+        }
+      }
+    };
+
+    this.setEntities([pointEntity]);
   }
 }
