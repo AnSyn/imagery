b158ce865a1ae1781ace0efa9ee3fd628f954cef
suzanz <suzanz@codevalue.net>
2020-07-08 15:07:47 +0300

    remove remarks

--- imagery-cesium/models/annotation-type.enum.ts
+++ imagery-cesium/models/annotation-type.enum.ts
@@ -2,8 +2,5 @@ export enum AnnotationType {
 	Point = 'Point',
 	LineString = 'LineString',
 	Polygon = 'Polygon',
-	// Circle = 'Circle',
 	Rectangle = 'Rectangle',
-	// Arrow = 'Arrow',
-	// Translate = 'Translate'
 }
