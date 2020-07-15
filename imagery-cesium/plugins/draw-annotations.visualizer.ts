ff0ccb93db21f6ae174caed9ae69ba6354e08082
suzanz <suzanz@codevalue.net>
2020-07-15 09:44:48 +0300

    draw circle for nathan

--- imagery-cesium/plugins/draw-annotations.visualizer.ts
+++ imagery-cesium/plugins/draw-annotations.visualizer.ts
@@ -1,5 +1,5 @@
 import { BaseImageryPlugin, IVisualizerEntity, ImageryPlugin, IVisualizerStateStyle, ANNOTATIONS_INITIAL_STYLE } from '@ansyn/imagery';
-import { Viewer, Cartesian3, Entity, Property, CallbackProperty, PolygonHierarchy, defined, ColorMaterialProperty, HeightReference, Color, Cartesian2, PolylineGeometry } from 'cesium';
+import { Viewer, Cartesian3, Entity, Property, CallbackProperty, PolygonHierarchy, defined, ColorMaterialProperty, HeightReference, Color, Cartesian2, Math as CesiumMath } from 'cesium';
 import { CesiumMap } from '../maps/cesium-map/cesium-map';
 import { Observable, Subscription, Subject } from 'rxjs';
 import { FeatureCollection, GeometryObject, Feature } from 'geojson';
@@ -45,10 +45,46 @@ export class CesiumDrawAnnotationsVisualizer extends BaseImageryPlugin {
 	events = {
 		onDrawEnd: new Subject<FeatureCollection<GeometryObject>>()
 	}
+	centerPixelX: number;
+	centerPixelY: number;
+
+	lastCirclePoistion;
 
 	constructor() {
 		super();
 	}
+
+	createEllipse(a /*pixels*/, b /*pixels*/, x = 0 /*pixels*/, y = 0 /*pixels*/, rotation = 0 /*degrees*/) {
+
+		rotation = rotation / 180 * Math.PI;
+		let n = Math.ceil(36 * (Math.max(a / b, b / a))); // n sampling angles, more for more elongated ellipses
+		let coords = [];
+		const cartesia3 = [];
+	  
+		for (let i = 0; i <= n; i++) {
+		  // get the current angle
+		  const θ = Math.PI * 2 / n * i + rotation;
+	  
+		  // get the radius at that angle
+		  const r = a * b / Math.sqrt(a * a * Math.sin(θ) * Math.sin(θ) + b * b * Math.cos(θ) *  Math.cos(θ));
+	  
+		  // get the x,y coordinate that marks the ellipse at this angle
+		  const x1 = x + Math.cos(θ - rotation) * r;
+		  const y1 = y + Math.sin(θ - rotation) * r;
+
+		  const coordinate = this.cesiumMap.getEarthPositionFromScreenPixels(new Cartesian2(x1, y1));
+		  cartesia3.push(coordinate);
+	  
+		//   coords.push([coordinate.x, coordinate.y, coordinate.z]);
+		}
+
+		coords = cartesianToCoordinates(cartesia3);
+	  
+		// return a geojson object:
+		return { "type": "Polygon", "coordinates": [coords] };
+	  
+	  }
+	
 
 	onInit() {
 		super.onInit();
@@ -95,6 +131,10 @@ export class CesiumDrawAnnotationsVisualizer extends BaseImageryPlugin {
 			const earthPosition = this.cesiumMap.getEarthPositionFromScreenPixels(screenPixels.position);
 			if (defined(earthPosition)) {
 				if (!this.activeShapePoints) {
+
+					this.centerPixelX = screenPixels.position.x;
+					this.centerPixelY = screenPixels.position.y;
+
 					this.activeShapePoints = [];
 
 					this.activeShapePoints.push(earthPosition);
@@ -119,6 +159,8 @@ export class CesiumDrawAnnotationsVisualizer extends BaseImageryPlugin {
 
 	private onMouseMoveEvent(pixelPoint: Cartesian2) {
 		const newEarthPosition = this.cesiumMap.getEarthPositionFromScreenPixels(pixelPoint);
+		this.lastCirclePoistion = pixelPoint;
+
 
 		if (!defined(this.floatingPoint)) {
 			this.floatingPoint = defined(newEarthPosition) ? this.addAnnotation(AnnotationType.Point, newEarthPosition) : undefined;
@@ -178,6 +220,9 @@ export class CesiumDrawAnnotationsVisualizer extends BaseImageryPlugin {
 						heightReference: HeightReference.CLAMP_TO_GROUND ,
 					},
 				});
+				break;
+			}
+			case AnnotationType.Circle: {
 			}
 		}
 		this.temporaryShapes.push(shape);
@@ -229,6 +274,16 @@ export class CesiumDrawAnnotationsVisualizer extends BaseImageryPlugin {
 					type: AnnotationMode.LineString,
 					coordinates: coordinates
 				};
+				break;
+			}
+			case AnnotationMode.Circle: {
+				const x1 = this.centerPixelX;
+				const y1 = this.centerPixelY;
+				const x2 = this.lastCirclePoistion.x;
+				const y2 = this.lastCirclePoistion.y;
+
+				const radius = Math.sqrt((Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
+				geometry = this.createEllipse(radius, radius, x1, y1, 0);
 				break;
 			}
 		} 
