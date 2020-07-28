c9185855fe58b37429065a43d93658ea9ff24d7c
Yehuda Buzaglo <yehudab@codevalue.com>
2020-07-28 16:53:24 +0300

    removing prettier

--- imagery-ol/mapSourceProviders/open-layers-GEE-source-provider.ts
+++ imagery-ol/mapSourceProviders/open-layers-GEE-source-provider.ts
@@ -1,83 +1,60 @@
-import XYZ from "ol/source/XYZ";
+import XYZ from 'ol/source/XYZ';
 import {
 	CacheService,
 	ImageryCommunicatorService,
 	ImageryMapSource,
-	IMapSettings,
-	IMapSourceProvidersConfig,
-	MAP_SOURCE_PROVIDERS_CONFIG,
-} from "@ansyn/imagery";
-import {
-	IMAGE_PROCESS_ATTRIBUTE,
-	OpenLayersMapSourceProvider,
-} from "./open-layers.map-source-provider";
-import { OpenLayersMap } from "../maps/open-layers-map/openlayers-map/openlayers-map";
-import { OpenLayersDisabledMap } from "../maps/openlayers-disabled-map/openlayers-disabled-map";
-import { HttpClient } from "@angular/common/http";
-import { Inject } from "@angular/core";
+	IMapSettings, IMapSourceProvidersConfig,
+	MAP_SOURCE_PROVIDERS_CONFIG
+} from '@ansyn/imagery';
+import { IMAGE_PROCESS_ATTRIBUTE, OpenLayersMapSourceProvider } from './open-layers.map-source-provider';
+import { OpenLayersMap } from '../maps/open-layers-map/openlayers-map/openlayers-map';
+import { OpenLayersDisabledMap } from '../maps/openlayers-disabled-map/openlayers-disabled-map';
+import { HttpClient } from '@angular/common/http';
+import { Inject } from '@angular/core';
 
-export const OpenLayerGEESourceProviderSourceType = "GEE";
+​
+export const OpenLayerGEESourceProviderSourceType = 'GEE';
+​
 @ImageryMapSource({
 	sourceType: OpenLayerGEESourceProviderSourceType,
-	supported: [OpenLayersMap, OpenLayersDisabledMap],
+	supported: [OpenLayersMap, OpenLayersDisabledMap]
 })
 export class OpenLayerGEESourceProvider extends OpenLayersMapSourceProvider {
+
 	layerData: any;
 
-	constructor(
-		protected httpClient: HttpClient,
-		protected cacheService: CacheService,
-		protected imageryCommunicatorService: ImageryCommunicatorService,
-		@Inject(MAP_SOURCE_PROVIDERS_CONFIG)
-		protected mapSourceProvidersConfig: IMapSourceProvidersConfig
-	) {
-		super(
-			cacheService,
-			imageryCommunicatorService,
-			mapSourceProvidersConfig
-		);
+	constructor(protected httpClient: HttpClient,
+				protected cacheService: CacheService,
+				protected imageryCommunicatorService: ImageryCommunicatorService,
+				@Inject(MAP_SOURCE_PROVIDERS_CONFIG) protected mapSourceProvidersConfig: IMapSourceProvidersConfig) {
+		super(cacheService, imageryCommunicatorService, mapSourceProvidersConfig);
 	}
 
 	async create(metaData: IMapSettings): Promise<any> {
 		const config = { ...this.config, ...metaData.data.config };
-
+		
 		this.layerData = await this.getLayersData(config.serverUrl);
 
 		const extent = this.createExtent(metaData);
 		const source = await this.createSource(metaData);
 		const tileLayer = this.createLayer(source, extent);
-		tileLayer.set(
-			IMAGE_PROCESS_ATTRIBUTE,
-			this.getImageLayer(source, extent)
-		);
+		tileLayer.set(IMAGE_PROCESS_ATTRIBUTE, this.getImageLayer(source, extent));
 		return Promise.resolve(tileLayer);
 	}
 
-	createSource(metaData: IMapSettings): any {
+	createSource(metaData: IMapSettings): any{
 		const config = { ...this.config, ...metaData.data.config };
-		const geeDefs = JSON.parse(
-			this.layerData.replace(/([\[\{,])\s*(\w+)\s*:/g, '$1 "$2":')
-		);
+		const geeDefs = JSON.parse(this.layerData.replace(/([\[\{,])\s*(\w+)\s*:/g, '$1 "$2":'));
 		const source = new XYZ({
-			url:
-				config.serverUrl +
-				`/query?request=` +
-				geeDefs.layers[0].requestType +
-				`&channel=` +
-				geeDefs.layers[0].id +
-				`&version=` +
-				geeDefs.layers[0].version +
-				`&x={x}&y={y}&z={z}`,
-			crossOrigin: "anonymous",
-			minZoom: 1,
+			url: config.serverUrl + `/query?request=` + geeDefs.layers[0].requestType + `&channel=` + geeDefs.layers[0].id + `&version=` + geeDefs.layers[0].version + `&x={x}&y={y}&z={z}`,
+			crossOrigin: 'anonymous',
+			minZoom: 1
 		});
-		return source;
+		return source
 	}
 
 	getLayersData(serverURL: string): Promise<any> {
 		const fileUrl = serverURL + `/query?request=Json&is2d=t`;
-		return this.httpClient
-			.get(fileUrl, { responseType: "text" })
-			.toPromise();
+		return this.httpClient.get(fileUrl, { responseType: 'text' }).toPromise();
 	}
 }
