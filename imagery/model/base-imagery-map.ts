7b4fbf13698dd0696ec9677419705c55405b5516
Suzi Zaher <suzanz@codevalue.net>
2021-08-25 11:56:06 +0300

    fixing peerdependency packages + updating version + enable ivy

--- imagery/model/base-imagery-map.ts
+++ imagery/model/base-imagery-map.ts
@@ -1,9 +1,14 @@
-import { EventEmitter, ViewContainerRef } from '@angular/core';
-import { Observable } from 'rxjs';
-import { GeoJsonObject, Point } from 'geojson';
-import { ImageryMapExtent, IImageryMapPosition, IMousePointerMove, IMouseClick } from './case-map-position.model';
-import { IMapErrorMessage, IMapProgress } from './map-progress.model';
-import { IBaseImageryLayer } from './imagery-layer.model';
+import { EventEmitter, ViewContainerRef } from "@angular/core";
+import { Observable } from "rxjs";
+import { GeoJsonObject, Point } from "geojson";
+import {
+	ImageryMapExtent,
+	IImageryMapPosition,
+	IMousePointerMove,
+	IMouseClick,
+} from "./case-map-position.model";
+import { IMapErrorMessage, IMapProgress } from "./map-progress.model";
+import { IBaseImageryLayer } from "./imagery-layer.model";
 
 export interface IImageryMapMetaData {
 	deps?: any[];
@@ -20,7 +25,7 @@ export interface ICanvasExportData {
 export interface IBaseImageryMapConstructor {
 	groupLayers: Map<string, IBaseImageryLayer>;
 
-	new(...args): BaseImageryMap;
+	new (...args): BaseImageryMap;
 }
 
 // @dynamic
@@ -30,17 +35,30 @@ export abstract class BaseImageryMap<T = any> {
 	readonly mapType?: string;
 	readonly defaultMapSource?: string;
 
-	public positionChanged: EventEmitter<IImageryMapPosition> = new EventEmitter<IImageryMapPosition>();
-	public mousePointerMoved: EventEmitter<IMousePointerMove> = new EventEmitter<IMousePointerMove>();
-	public mouseSingleClick: EventEmitter<IMouseClick> = new EventEmitter<IMouseClick>();
-	public mouseRightClick: EventEmitter<IMouseClick> = new EventEmitter<IMouseClick>();
-	public mouseDoubleClick: EventEmitter<IMouseClick> = new EventEmitter<IMouseClick>();
-	public moveStart: EventEmitter<IImageryMapPosition> = new EventEmitter<IImageryMapPosition>();
-	public mapLayerChangedEventEmitter: EventEmitter<any> = new EventEmitter<any>();
+	public positionChanged: EventEmitter<IImageryMapPosition> =
+		new EventEmitter<IImageryMapPosition>();
+	public mousePointerMoved: EventEmitter<IMousePointerMove> =
+		new EventEmitter<IMousePointerMove>();
+	public mouseSingleClick: EventEmitter<IMouseClick> =
+		new EventEmitter<IMouseClick>();
+	public mouseRightClick: EventEmitter<IMouseClick> =
+		new EventEmitter<IMouseClick>();
+	public mouseDoubleClick: EventEmitter<IMouseClick> =
+		new EventEmitter<IMouseClick>();
+	public moveStart: EventEmitter<IImageryMapPosition> =
+		new EventEmitter<IImageryMapPosition>();
+	public mapLayerChangedEventEmitter: EventEmitter<any> =
+		new EventEmitter<any>();
 
-	public tilesLoadProgressEventEmitter: EventEmitter<IMapProgress> = new EventEmitter<IMapProgress>();
-	public tilesLoadErrorEventEmitter: EventEmitter<IMapErrorMessage> = new EventEmitter<IMapErrorMessage>();
-	public mapObject: T;
+	public tilesLoadProgressEventEmitter: EventEmitter<IMapProgress> =
+		new EventEmitter<IMapProgress>();
+	public tilesLoadErrorEventEmitter: EventEmitter<IMapErrorMessage> =
+		new EventEmitter<IMapErrorMessage>();
+
+	protected _mapObject: T;
+	get mapObject() {
+		return this._mapObject;
+	}
 
 	abstract getCenter(): Observable<Point>;
 
@@ -48,27 +66,38 @@ export abstract class BaseImageryMap<T = any> {
 
 	abstract toggleGroup(groupName: string, newState: boolean);
 
-	abstract initMap(element: HTMLElement, shadowNorthElement: HTMLElement, shadowDoubleBufferElement: HTMLElement, layer?: IBaseImageryLayer, position?: IImageryMapPosition, mapViewContainerRef?: ViewContainerRef): Observable<boolean>;
+	abstract initMap(
+		element: HTMLElement,
+		shadowNorthElement: HTMLElement,
+		shadowDoubleBufferElement: HTMLElement,
+		layer?: IBaseImageryLayer,
+		position?: IImageryMapPosition,
+		mapViewContainerRef?: ViewContainerRef
+	): Observable<boolean>;
 
 	// This method is for the use of the @AutoSubscription decorator
-	initMapSubscriptions(): void {
-	};
+	initMapSubscriptions(): void {}
 
 	/**
 	 * @description Reset the Map view with a new view with the new layer projection (NOTE: also Delete's previous layers)
 	 * @param layer The new layer to set the view with. this layer projection will be the views projection
 	 * @param extent The extent (bounding box points) of the map at ESPG:4326
 	 */
-	abstract resetView(layer: IBaseImageryLayer, position: IImageryMapPosition, extent?: ImageryMapExtent, useDoubleBuffer?: boolean): Observable<boolean>;
+	abstract resetView(
+		layer: IBaseImageryLayer,
+		position: IImageryMapPosition,
+		extent?: ImageryMapExtent,
+		useDoubleBuffer?: boolean
+	): Observable<boolean>;
 
 	abstract addLayer(layer: IBaseImageryLayer): void;
 
 	addMapLayer(layer: IBaseImageryLayer): void {
-		throw new Error('Method not implemented.');
-	};
+		throw new Error("Method not implemented.");
+	}
 
 	getMainLayer(): IBaseImageryLayer {
-		throw new Error('Method not implemented.');
+		throw new Error("Method not implemented.");
 	}
 
 	abstract getLayers(): IBaseImageryLayer[];
@@ -97,17 +126,20 @@ export abstract class BaseImageryMap<T = any> {
 
 	abstract addLayerIfNotExist(layer: IBaseImageryLayer);
 
-	abstract getCoordinateFromScreenPixel(screenPixel: { x, y }): [number, number, number];
+	abstract getCoordinateFromScreenPixel(screenPixel: {
+		x;
+		y;
+	}): [number, number, number];
 
 	abstract getHtmlContainer(): HTMLElement;
 
 	abstract getProjectionCode(): string;
 
 	fitToExtent(extent: any): Observable<any> {
-		throw new Error('Method not implemented.');
-	};
+		throw new Error("Method not implemented.");
+	}
 
 	getExtraData(): { [key: string]: any } {
-		return {}
+		return {};
 	}
 }
--- imagery/public_api.ts
+++ imagery/public_api.ts
@@ -1,35 +1,48 @@
 export { MapOrientation } from "./model/map-settings";
-export { IMapSource } from './model/map-providers-config';
-export { IStroke, IIcon } from './model/visualizers/visualizer-style';
-export { ICanvasExportData } from './model/base-imagery-map';
+export { IMapSource } from "./model/map-providers-config";
+export { IStroke, IIcon } from "./model/visualizers/visualizer-style";
+export { ICanvasExportData } from "./model/base-imagery-map";
 export {
-	ImageryMapExtent, ImageryMapExtentPolygon, IImageryMapProjectedState, IImageryMapPosition, IMousePointerMove, IMouseClick
-}from './model/case-map-position.model';
+	ImageryMapExtent,
+	ImageryMapExtentPolygon,
+	IImageryMapProjectedState,
+	IImageryMapPosition,
+	IMousePointerMove,
+	IMouseClick,
+} from "./model/case-map-position.model";
 
-export { MAP_SOURCE_PROVIDERS_CONFIG, IMapSourceProvidersConfig } from './model/base-map-source-provider';
-export { MarkerSizeDic } from './model/visualizers/visualizer-style';
-export { IVisualizerEntity } from './model/visualizers/visualizers-entity';
-export { MarkerSize } from './model/visualizers/visualizer-style';
-export { IVisualizerStyle } from './model/visualizers/visualizer-style';
-export { IVisualizerStateStyle } from './model/visualizers/visualizer-state';
-export { VisualizerStates, ANNOTATIONS_INITIAL_STYLE, ANNOTATIONS_FEATURE_INITIAL_PROPERTIES } from './model/visualizers/visualizer-state';
-export { IMapProgress, IMapErrorMessage } from './model/map-progress.model';
+export {
+	MAP_SOURCE_PROVIDERS_CONFIG,
+	IMapSourceProvidersConfig,
+} from "./model/base-map-source-provider";
+export { MarkerSizeDic } from "./model/visualizers/visualizer-style";
+export { IVisualizerEntity } from "./model/visualizers/visualizers-entity";
+export { MarkerSize } from "./model/visualizers/visualizer-style";
+export { IVisualizerStyle } from "./model/visualizers/visualizer-style";
+export { IVisualizerStateStyle } from "./model/visualizers/visualizer-state";
+export {
+	VisualizerStates,
+	ANNOTATIONS_INITIAL_STYLE,
+	ANNOTATIONS_FEATURE_INITIAL_PROPERTIES,
+} from "./model/visualizers/visualizer-state";
+export { IMapProgress, IMapErrorMessage } from "./model/map-progress.model";
 export {
 	IMapProviderConfig,
 	IMapProvidersConfig,
-	MAP_PROVIDERS_CONFIG
-} from './model/map-providers-config';
-export { IImageryMapSources } from './providers/map-source-providers';
-export { IMapInstanceChanged } from './communicator-service/communicator.entity';
-export { IBaseMapSourceProviderConstructor } from './model/base-map-source-provider';
-export { VisualizerInteractions } from './model/base-imagery-visualizer';
-export { IBaseImageryMapConstructor } from './model/base-imagery-map';
+	MAP_PROVIDERS_CONFIG,
+} from "./model/map-providers-config";
+export { IImageryMapSources } from "./providers/map-source-providers";
+export { IMapInstanceChanged } from "./communicator-service/communicator.entity";
+export { IBaseMapSourceProviderConstructor } from "./model/base-map-source-provider";
+export { VisualizerInteractions } from "./model/base-imagery-visualizer";
+export { IBaseImageryMapConstructor } from "./model/base-imagery-map";
 export {
 	createImageryMapsCollection,
 	IMAGERY_MAPS,
 	IMAGERY_MAPS_COLLECTIONS,
-	ImageryMapsFactory, ImageryMapsProvider
-} from './providers/imagery-map-collection';
+	ImageryMapsFactory,
+	ImageryMapsProvider,
+} from "./providers/imagery-map-collection";
 export {
 	areCoordinatesNumeric,
 	bboxFromGeoJson,
@@ -51,32 +64,55 @@ export {
 	getDistanceBetweenPoints,
 	getNewPoint,
 	EPSG_4326,
-	EPSG_3857
-} from './utils/geo';
+	EPSG_3857,
+} from "./utils/geo";
 
-export { IVisualizersConfig, VisualizersConfig } from './model/visualizers-config.token';
-export { ImageryVisualizer } from './decorators/imagery-visualizer';
-export { BaseImageryPlugin, IImageryPluginMetaData } from './model/base-imagery-plugin';
+export {
+	IVisualizersConfig,
+	VisualizersConfig,
+} from "./model/visualizers-config.token";
+export { ImageryVisualizer } from "./decorators/imagery-visualizer";
+export {
+	BaseImageryPlugin,
+	IImageryPluginMetaData,
+} from "./model/base-imagery-plugin";
 export {
 	BaseImageryVisualizer,
 	IBaseImageryVisualizerClass,
-	IImageryVisualizerMetaData, VisualizerInteractionTypes
-} from './model/base-imagery-visualizer';
-export { ImageryPlugin } from './decorators/imagery-plugin';
-export { ImageryMap } from './decorators/imagery-map';
-export { ImageryMapSource } from './decorators/map-source-provider';
-export { BaseImageryMap } from './model/base-imagery-map';
-export { BaseMapSourceProvider } from './model/base-map-source-provider';
-export { CacheService } from './cache-service/cache.service';
-export { ImageryCommunicatorService } from './communicator-service/communicator.service';
-export { CommunicatorEntity } from './communicator-service/communicator.entity';
-export { ProjectionService } from './projection-service/projection.service';
-export { StayInImageryService } from './stay-in-imagery-service/stay-in-imagery.service';
-export { ImageryModule } from './imagery.module';
-export { IMAGERY_CONFIG } from './model/configuration.token';
-export { IBaseImageryLayer, ImageryLayerProperties, IMAGERY_MAIN_LAYER_NAME, IMAGERY_BASE_MAP_LAYER } from './model/imagery-layer.model';
+	IImageryVisualizerMetaData,
+	VisualizerInteractionTypes,
+} from "./model/base-imagery-visualizer";
+export { ImageryPlugin } from "./decorators/imagery-plugin";
+export { ImageryMap } from "./decorators/imagery-map";
+export { ImageryMapSource } from "./decorators/map-source-provider";
+export { BaseImageryMap } from "./model/base-imagery-map";
+export { BaseMapSourceProvider } from "./model/base-map-source-provider";
+export { CacheService } from "./cache-service/cache.service";
+export { ImageryCommunicatorService } from "./communicator-service/communicator.service";
+export { CommunicatorEntity } from "./communicator-service/communicator.entity";
+export { ProjectionService } from "./projection-service/projection.service";
+export { StayInImageryService } from "./stay-in-imagery-service/stay-in-imagery.service";
+export { ImageryModule } from "./imagery.module";
+export { IMAGERY_CONFIG } from "./model/configuration.token";
+export {
+	IBaseImageryLayer,
+	ImageryLayerProperties,
+	IMAGERY_MAIN_LAYER_NAME,
+	IMAGERY_BASE_MAP_LAYER,
+} from "./model/imagery-layer.model";
 
-export { IMapSettings, IMapSettingsData, IWorldViewMapState } from './model/map-settings';
-export { toDegrees, toRadians, getAngleDegreeBetweenPoints, getAngleDegreeBetweenCoordinates } from './utils/math';
-export { ExtentCalculator } from './utils/extent-calculator';
-export { getOpacityFromColor } from './utils/color-opacity-extractor';
+export {
+	IMapSettings,
+	IMapSettingsData,
+	IWorldViewMapState,
+} from "./model/map-settings";
+export {
+	toDegrees,
+	toRadians,
+	getAngleDegreeBetweenPoints,
+	getAngleDegreeBetweenCoordinates,
+} from "./utils/math";
+export { ExtentCalculator } from "./utils/extent-calculator";
+export { getOpacityFromColor } from "./utils/color-opacity-extractor";
+
+export { ImageryComponent } from "./imagery/imagery.component";
